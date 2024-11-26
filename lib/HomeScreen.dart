d3362442e86544d89fd5e83fc210ba834d7da0ad
jinukrishnan <jinukrishnan.p@gmail.com>
2024-11-26 15:13:45 +0530

    asd

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,47 @@
+import 'package:appwrite_userauth/AppwriteServices.dart';
+import 'package:appwrite_userauth/LogIn.dart';
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late AppwriteServices _appwriteService;
+
+  void initState() {
+    super.initState();
+    _appwriteService = AppwriteServices();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Screen"),
+        actions: [
+          GestureDetector(
+            onTap: () async {
+              try {
+                await _appwriteService.logoutUser();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                );
+              } catch (e) {
+                print(e);
+              }
+            },
+
+            child: Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: Center(child: Text("Home Screen")),
+    );
+  }
+}
